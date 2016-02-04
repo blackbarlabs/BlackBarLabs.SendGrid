c929e4d806bba657c517619cb21b4d508723c20d
Keith Holloway <keith@eastfive.com>
2016-02-04 12:31:00 -0600

    Genesis

---
+++ BlackBarLabs.SendGrid.Tests/MailerTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BlackBarLabs.SendGrid.Exceptions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BlackBarLabs.SendGrid.Tests
+{
+    [TestClass]
+    public class MailerTest
+    {
+        [Ignore]
+        [TestMethod]
+        public async Task SendEmail()
+        {
+            var username = System.Configuration.ConfigurationManager.AppSettings["SendGrid.UserName"];
+            var password = System.Configuration.ConfigurationManager.AppSettings["SendGrid.Password"];
+            var mailer = new Mailer(username, password);
+            var html = @"<h1>Test Message</h1><p>This is a test</p>" ;
+            await mailer.SendEmailMessageAsync("keithdholloway@gmail.com", "test@blackbarlabs.com", "Black Bar Labs Testing",
+                "Test Email", html);
+        }
+
+        [Ignore]
+        [TestMethod]
+        public async Task SendEmailWithSubstitutionParameters()
+        {
+            var username = System.Configuration.ConfigurationManager.AppSettings["SendGrid.UserName"];
+            var password = System.Configuration.ConfigurationManager.AppSettings["SendGrid.Password"];
+            var mailer = new Mailer(username, password);
+            var html = @"<h1>Test Message</h1><p>This is a test</p><br/><p>-Name-</p><br/><p>-Number-</p><br/><p>-State-</p><br/>";
+
+            var substitutions = new Dictionary<string, List<string>>();
+            substitutions.Add("-Name-", new List<string>() {"John"});
+            substitutions.Add("-Number-", new List<string>() { "123123" });
+            substitutions.Add("-State-", new List<string>() { "GA" });
+            
+            await mailer.SendEmailMessageAsync("keithdholloway@gmail.com", "test@blackbarlabs.com", "Black Bar Labs Testing",
+                "Test Email", html, substitutions);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(EmailSubstitutionParameterException))]
+        public async Task SendEmailWithTooManySubstitutionParameters()
+        {
+            var username = System.Configuration.ConfigurationManager.AppSettings["SendGrid.UserName"];
+            var password = System.Configuration.ConfigurationManager.AppSettings["SendGrid.Password"];
+            var mailer = new Mailer(username, password);
+            var html = @"<h1>Test Message</h1><p>This is a test</p><br/><p>-Name-</p><br/><p>-Number-</p><br/><p>-State-</p><br/>";
+
+            var substitutions = new Dictionary<string, List<string>>();
+            substitutions.Add("-Name-", new List<string>() { "John" });
+            substitutions.Add("-Number-", new List<string>() { "123123" });
+            substitutions.Add("-State-", new List<string>() { "GA" });
+            substitutions.Add("-Missing-", new List<string>() { "Missing" });
+
+            await mailer.SendEmailMessageAsync("keithdholloway@gmail.com", "test@blackbarlabs.com", "Black Bar Labs Testing",
+                "Test Email", html, substitutions);
+        }
+    }
+}
---
+++ BlackBarLabs.SendGrid/Mailer.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using BlackBarLabs.SendGrid.Exceptions;
+using Exceptions;
+using SendGrid;
+
+namespace BlackBarLabs.SendGrid
+{
+    public class Mailer : Web.ISendMailService
+    {
+        private readonly string username;
+        private readonly string password;
+        public Mailer(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+        public async Task SendEmailMessageAsync(string toAddress, string fromAddress, string fromName, string subject, string html, IDictionary<string, List<string>> substitutions = null)
+        {
+            // Create the email object first, then add the properties.
+            var myMessage = new SendGridMessage();
+            myMessage.AddTo(toAddress);
+            myMessage.From = new MailAddress(fromAddress, fromName);
+            myMessage.Subject = subject;
+            myMessage.Html = html;
+
+            ValidateMessageSubstitutions(html, substitutions);
+            if (default(IDictionary<string, List<string>>) != substitutions)
+            {
+                foreach (var substitutionsKvp in substitutions)
+                    myMessage.AddSubstitution(substitutionsKvp.Key, substitutionsKvp.Value);
+            }
+
+            // Create credentials, specifying your user name and password.
+            var credentials = new NetworkCredential(username, password);
+
+            // Create an Web transport for sending email.
+            var transportWeb = new global::SendGrid.Web(credentials);
+
+            // Send the email, which returns an awaitable task.
+            try
+            {
+                await transportWeb.DeliverAsync(myMessage);
+            }
+            catch (InvalidApiRequestException ex)
+            {
+                var details = new StringBuilder();
+
+                details.Append("ResponseStatusCode: " + ex.ResponseStatusCode + ".   ");
+                for (int i = 0; i < ex.Errors.Count(); i++)
+                {
+                    details.Append(" -- Error #" + i.ToString() + " : " + ex.Errors[i]);
+                }
+
+                throw new ApplicationException(details.ToString(), ex);
+            }
+        }
+
+        private static void ValidateMessageSubstitutions(string html, IDictionary<string, List<string>>substitutions)
+        {
+            if (null == substitutions) return;
+            foreach (var sub in substitutions)
+            {
+                if(!html.Contains(sub.Key))
+                    throw new EmailSubstitutionParameterException("Could not find substitution string " + sub.Key + " in email text.");
+                if (null == sub.Value)
+                    throw new EmailSubstitutionParameterException("No list of substitutions given for substitution value " + sub.Key);
+                if (sub.Value.Count == 0)
+                    throw new EmailSubstitutionParameterException("No value given for substitution value " + sub.Key);
+                var values = sub.Value;
+                foreach (var value in values)
+                {
+                    if (string.IsNullOrEmpty(value))
+                        throw new EmailSubstitutionParameterException("String is null for substitution value " + sub.Key);
+                }
+            }
+        }
+    }
+}
