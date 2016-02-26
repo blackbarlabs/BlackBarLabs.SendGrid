ad76205c0e4d66599dcfd06e909689e938650cd9
Keith Holloway <keith@eastfive.com>
2016-02-26 15:46:14 -0600

    Added test to send email from text file

--- BlackBarLabs.SendGrid.Tests/MailerTest.cs
+++ BlackBarLabs.SendGrid.Tests/MailerTest.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 using System.Threading.Tasks;
 using BlackBarLabs.SendGrid.Exceptions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -18,6 +20,25 @@ namespace BlackBarLabs.SendGrid.Tests
             var mailer = new Mailer(username, password);
             var html = @"<h1>Test Message</h1><p>This is a test</p>" ;
             await mailer.SendEmailMessageAsync("keithdholloway@gmail.com", "test@blackbarlabs.com", "Black Bar Labs Testing",
+                "Test Email", html);
+        }
+
+        [Ignore]
+        [TestMethod]
+        public async Task SendEmailFromFile()
+        {
+            var username = System.Configuration.ConfigurationManager.AppSettings["SendGrid.UserName"];
+            var password = System.Configuration.ConfigurationManager.AppSettings["SendGrid.Password"];
+            var mailer = new Mailer(username, password);
+
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "BlackBarLabs.SendGrid.Tests.email.txt";
+            var stream = assembly.GetManifestResourceStream(resourceName);
+            var reader = new StreamReader(stream);
+            var template = reader.ReadToEnd();
+
+            var html = template;
+            await mailer.SendEmailMessageAsync("keith@eastfive.com", "test@eastfive.com", "Testing",
                 "Test Email", html);
         }
 
