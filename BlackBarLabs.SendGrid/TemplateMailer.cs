9a7f9e44e81b12e19985a631133d9a3713b6fc11
Josh <josh@eastfive.com>
2017-01-18 21:31:25 -0600

    updated template mailer for new services

--- BlackBarLabs.SendGrid/TemplateMailer.cs
+++ BlackBarLabs.SendGrid/TemplateMailer.cs
@@ -8,6 +8,7 @@ using SendGrid;
 using System.Net.Mail;
 using System.Net;
 using Exceptions;
+using BlackBarLabs.Extensions;
 
 namespace BlackBarLabs.SendGrid
 {
@@ -20,6 +21,68 @@ namespace BlackBarLabs.SendGrid
         {
             this.username = username;
             this.password = password;
+        }
+
+        public async Task<TResult> SendEmailMessageAsync<TResult>(
+            string toAddress, string toName, 
+            string fromAddress, string fromName,
+            string templateName, 
+            IDictionary<string, string> substitutionsSingle,
+            IDictionary<string, string[]> substitutionsMultiple, 
+            Func<string, TResult> onSuccess, 
+            Func<TResult> onServiceUnavailable,
+            Func<string, TResult> onFailed)
+        {
+            var subject = substitutionsSingle.ContainsKey("subject") ?
+                substitutionsSingle["subject"]
+                :
+                "";
+
+            var myMessage = new SendGridMessage();
+            myMessage.AddTo(toAddress);
+            myMessage.From = new MailAddress(fromAddress, fromName);
+            myMessage.Subject = subject;
+            myMessage.EnableTemplateEngine(templateName);
+            myMessage.Text = "asdf";
+            myMessage.Html = "<html></html>";
+
+            if (default(IDictionary<string, string>) != substitutionsSingle)
+            {
+                foreach (var substitutionsKvp in substitutionsSingle)
+                    myMessage.AddSubstitution(substitutionsKvp.Key,
+                        substitutionsKvp.Value.ToEnumerable().ToList());
+            }
+
+            if (default(IDictionary<string, List<string>>) != substitutionsMultiple)
+            {
+                foreach (var substitutionsKvp in substitutionsMultiple)
+                    myMessage.AddSubstitution(substitutionsKvp.Key, substitutionsKvp.Value.ToList());
+            }
+
+            // Create credentials, specifying your user name and password.
+            var credentials = new NetworkCredential(username, password);
+
+            // Create an Web transport for sending email.
+            var transportWeb = new global::SendGrid.Web(credentials);
+
+            // Send the email, which returns an awaitable task.
+            try
+            {
+                await transportWeb.DeliverAsync(myMessage);
+            }
+            catch (InvalidApiRequestException ex)
+            {
+                var details = new StringBuilder();
+
+                details.Append("ResponseStatusCode: " + ex.ResponseStatusCode + ".   ");
+                for (int i = 0; i < ex.Errors.Count(); i++)
+                {
+                    details.Append(" -- Error #" + i.ToString() + " : " + ex.Errors[i]);
+                }
+
+                return onFailed(details.ToString());
+            }
+            return onSuccess.Invoke(toAddress);
         }
 
         public async Task SendEmailMessageAsync(string toAddress, string fromAddress, string fromName, string subject, 
@@ -66,11 +129,6 @@ namespace BlackBarLabs.SendGrid
                 throw new ApplicationException(details.ToString(), ex);
             }
             onSuccess.Invoke(toAddress);
-        }
-
-        public Task<TResult> SendEmailMessageAsync<TResult>(string toAddress, string toName, string fromAddress, string fromName, string templateName, IDictionary<string, string> substitutionsSingle, IDictionary<string, string[]> substitutionsMultiple, Func<string, TResult> onSuccess, Func<TResult> onServiceUnavailable, Func<string, TResult> onFailed)
-        {
-            throw new NotImplementedException();
         }
     }
 }
