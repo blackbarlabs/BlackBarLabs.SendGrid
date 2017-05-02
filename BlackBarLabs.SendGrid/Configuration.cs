04457905f63c10b63dc49c9dca124c66c791afce
Josh <josh@eastfive.com>
2017-05-02 13:19:53 -0500

    new template mailing methods

---
+++ BlackBarLabs.SendGrid/Configuration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBarLabs.SendGrid
+{
+    public static class Configuration
+    {
+        public const string ApiKey = "EastFive.SendGrid.ApiKey";
+        public const string MuteEmailToAddress = "EastFive.SendGrid.MuteToAddress";
+        public const string BccAllAddresses = "EastFive.SendGrid.BlindCopyAllAddresses";
+    }
+}
